fbe910059168eae04ad314c6c2d8602280e29a27
plutasnyy <kamil.plucinski97@gmail.com>
2017-05-11 14:33:09 +0200

    create first alghoritm, to do: improve output structure

---
+++ AW.py
@@ -0,0 +1,31 @@
+def return_bin(i,quantity_of_items):
+    number=bin(i)[2:] #number as binary
+    combination=[0 for i in range(quantity_of_items)]
+    for i in range(len(number)):
+        combination[-i-1]+=int(number[-i-1])
+    return combination
+
+def calculate_solution(X,thief):
+    value=0
+    size=0
+    for i in range(len(thief.item_list)):
+        value+=thief.item_list[i].value*X[i]
+        size+=thief.item_list[i].size*X[i]
+    return value,size
+
+def AW(thief):
+    max_value=0
+    best_solution=[]
+
+    quantity_of_items=len(thief.item_list)
+    combinations_number=2**quantity_of_items
+
+    for i in range(combinations_number):
+        X=return_bin(i,quantity_of_items)
+        current_value,size=calculate_solution(X,thief)
+        
+        if current_value > max_value and size<=thief.knapsack.capacity:
+            max_value=current_value
+            best_solution=X
+
+    print("Value: ",max_value,"X: ", best_solution)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from load_data import *
 from class_file import *
+from AW import *
 def print_input(thief):
     print(thief.knapsack.capacity)
     for i in thief.item_list:
         print(i.value,i.size)
+    print()
+
 ruckasck_capacity,items=load_data()
 thief=Thief(Knapsack(ruckasck_capacity),items)
 print_input(thief)
+
+
+AW(thief)
