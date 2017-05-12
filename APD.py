a9c6775d677fd45a99e45a1c551badc8ed09e992
plutasnyy <kamil.plucinski97@gmail.com>
2017-05-12 12:59:20 +0200

    repair fill empy row

--- APD.py
+++ APD.py
@@ -1,8 +1,8 @@
 def generate_empty_matrix(thief):
     temp=[]
-    for i in range(len(thief.item_list)):
+    for i in range(len(thief.item_list)+1):
         X=[]
-        for j in range(thief.knapsack.capacity):
+        for j in range(thief.knapsack.capacity+1):
             X.append(0)
         temp.append(X)
     return temp
@@ -14,20 +14,20 @@ def print_matrix(matrix):
 
 def fill_first_row(matrix,thief):
     item = thief.item_list[0]
-    for i in range(len(matrix[0])):
-        if i+1 < item.size:
-            matrix[0][i]=0
+    for i in range(1,len(matrix[0])):
+        if i < item.size:
+            matrix[1][i]=0
         else:
-            matrix[0][i]=item.value
+            matrix[1][i]=item.value
 
 def fill_empty_row_in_matrix(matrix,thief):
     for i in range(1,len(thief.item_list)):
         item=thief.item_list[i]
         for j in range(len(matrix[1])):
-            if j+1<item.size:
-                matrix[i][j]=matrix[i-1][j]
+            if j<item.size:
+                matrix[i+1][j]=matrix[i][j]
             else:
-                matrix[i][j]=max(matrix[i-1][j],matrix[i-1][j-item.size+1]+item.value)
+                matrix[i+1][j]=max(matrix[i][j-item.size]+item.value,matrix[i][j])
 
 def APD(thief):
     matrix=generate_empty_matrix(thief)
